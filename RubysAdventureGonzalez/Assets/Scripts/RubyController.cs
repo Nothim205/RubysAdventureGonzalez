20da12b7c8b6143edaa004014764920ea113d906
Nothim205 <116131975+Nothim205@users.noreply.github.com>
2022-12-01 11:44:33 -0800

    prefabs almost done
    turned things to prefabs and added box colliders.

--- RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
+++ RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+  Rigidbody2D rigidbody2d
+  float horizontal;
+  float vertical;
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,9 +17,12 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
